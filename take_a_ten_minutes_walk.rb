28e026478d179c81cd97167f3c7cae0c9e0da6b6
Nikita <nikitka_bodr@mail.ru>
2022-11-04 13:36:50 +0300

    remark

--- take_a_ten_minutes_walk.rb
+++ take_a_ten_minutes_walk.rb
@@ -3,17 +3,17 @@
 # arraydiff
 
 class CreditCardMask
-    attr_accessor :walk
-  
-    def initialize(walk)
-      @walk = walk
-    end
-  
-    def call
-        if(walk.count('n') == walk.count('s') and walk.count('w') == walk.count('e') and walk.length == 10)
-            return true
-        else
-            return false
-        end
+  attr_accessor :walk
+
+  def initialize(walk)
+    @walk = walk
+  end
+
+  def call
+    if (walk.count('n') == walk.count('s')) && (walk.count('w') == walk.count('e')) && (walk.length == 10)
+      true
+    else
+      false
     end
   end
+end
